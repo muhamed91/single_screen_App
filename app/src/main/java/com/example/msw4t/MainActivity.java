ec9a3e6c9e10c126be37de25aa1702fb562cc87f
muhamed91 <r_muhamed@live.com>
2020-07-26 12:57:49 +0200

    initial commit for the single screen App

---
+++ app/src/main/java/com/example/msw4t/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.msw4t;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
